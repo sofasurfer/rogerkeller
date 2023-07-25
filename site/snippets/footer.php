be7c41d753af543246af73c99d8b0ad991df3cb1
sofasurfer <kilian@sofasurfer.ch>
2023-07-25 15:22:22 +0200

    Update PIWIK code

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -15,20 +15,21 @@
   <?php echo js('assets/js/jquery.fullpage.min.js') ?>
   <?php echo js('assets/js/default.js?v1.0') ?>
 
-  <!-- Piwik -->
-  <script type="text/javascript">
-    var _paq = _paq || [];
+  <!-- Matomo -->
+  <script>
+    var _paq = window._paq = window._paq || [];
     /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
     _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
       var u="//piwik.sofasurfer.org/";
-      _paq.push(['setTrackerUrl', u+'piwik.php']);
-      _paq.push(['setSiteId', '31']);
+      _paq.push(['setTrackerUrl', u+'matomo.php']);
+      _paq.push(['setSiteId', '47']);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+      g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
     })();
   </script>
-  <!-- End Piwik Code -->  
+  <!-- End Matomo Code -->
+
 </body>
 </html>
